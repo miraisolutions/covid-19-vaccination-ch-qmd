1f015985eb9bd7e81aafce6e02643471e549f868
GuidoMaggio <guido.maggio@mirai-solutions.com>
2023-07-14 23:09:06 +0200

    Addd deploy step

---
+++ deploy/deploy-shinyapps.R
@@ -0,0 +1,44 @@
+# deploy/deploy-shinyapps.R
+# usethis::use_build_ignore("deploy")
+rsconnect::setAccountInfo(
+  Sys.getenv("SHINYAPPS_ACCOUNT"),
+  Sys.getenv("SHINYAPPS_TOKEN"),
+  Sys.getenv("SHINYAPPS_SECRET")
+)
+
+
+
+# quarto::quarto_publish_app(server = "shinyapps.io", 
+#                    name = "report", 
+#                    title = "Covid19 Vaccination CH",
+#                    account = "gmaggio",
+#                    render = "local" 
+#                    )
+
+quarto::quarto_publish_app(
+                            input = "Index.qmd",
+                           server = "shinyapps.io", 
+                           name = "covid-19-vaccination-ch-qmd", 
+                           title = "Covid19 Vaccination CH",
+                           account = "miraisolutions",
+                           render = "local"
+)
+
+# unlink("index.Rmd")
+
+# rsconnect::deployApp(
+#   appDir = "inst/report",
+#   appPrimaryDoc = "inst/report/index.Rmd",
+#   appName = "covid19-vaccination-ch",
+# )
+# rsconnect::deployDoc(
+#   doc = "inst/report/index.Rmd",
+#   appDir = ".",
+#   appName = "covid19-vaccination-ch",
+# )
+# rsconnect::deployApp(
+#   appDir = ".",
+#   appFiles = list.files(),
+#   appPrimaryDoc = "inst/report/index.Rmd",
+#   appName = "covid19-vaccination-ch"
+# )
